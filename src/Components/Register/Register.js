82921788596ce588ece2d441f2305c75f100f74d
shravani.gunjal <shravani.gunjal05@gmail.com>
2025-09-04 15:45:40 +0530

    called API in registration form

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,12 +1,42 @@
 import { useState } from "react"
 import "./Register.css"
 import { useNavigate } from "react-router-dom"
+import axios from 'axios'
 function Register(){
 const navigate=useNavigate()
     const navigateToDashBoard=()=>{
     navigate("/")
 }
+const [userData,setuserData]=useState({name:"", email:"", password:""})
 
+function handleRegistrationData(){
+    console.log(userData)
+    axios.post('http://localhost:3001/user', userData)
+    .then((singleElement)=>{
+        console.log('User Registered:', singleElement.data);
+        navigate("/login")
+        setuserData({
+            name:'',
+            email:'',
+            password:''
+        });
+    })
+}
+function handleName(event){
+    let user={...userData}
+    user["name"]=event.target.value
+    setuserData(user)
+}
+function handleEmail(event){
+    let user={...userData}
+    user["email"]=event.target.value
+    setuserData(user)
+}
+function handlePass(event){
+    let user={...userData}
+    user["password"]=event.target.value
+    setuserData(user)
+}
     return(
         <div className="background">
         <div className="header">
@@ -22,12 +52,12 @@ const navigate=useNavigate()
                 <div className="blogsDes">Publish your passions, your way ...</div>
                 <div className="login">Register</div>
                  <div className="inputText">Name</div>
-                <input type='text' placeholder="Firstname Lastname" className="textBox"/>
+                <input type='text' placeholder="Firstname Lastname" className="textBox" value={userData.name} onChange={handleName}/>
                 <div className="inputText">Email id</div>
-                <input type='text' placeholder="test@gmail.com" className="textBox"/>
+                <input type='text' placeholder="test@gmail.com" className="textBox" value={userData.email} onChange={handleEmail}/>
                 <div className="inputText">Password</div>
-                <input type="password" placeholder="Test@123" className="textBox"/>
-                <div><button className="registerButton">Register</button></div>
+                <input type="password" placeholder="Test@123" className="textBox" value={userData.password} onChange={handlePass}/>
+                <div><button className="registerButton" onClick={handleRegistrationData}>Register</button></div>
             </div>
         </div>
     </div>
--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -6,13 +6,18 @@ function Title() {
      const navigateToDashBoard=()=>{
     navigate("/")
 }
-const [userTitle,setuserTitle]=useState({Title:""})
+const [userTitle,setuserTitle]=useState({Title:"", Desc:""})
 function handleTitleData(){
     console.log(userTitle)
 }
 function handleTitle(event){
     let user={...userTitle}
     user["Title"]=event.target.value
+    setuserTitle(user)
+}
+function handleDesc(event){
+    let user={...userTitle}
+    user["Desc"]=event.target.value
     setuserTitle(user)
 }
     return (
@@ -26,7 +31,7 @@ function handleTitle(event){
             </div>
             <div className="titleBox">
                 <input type="text" placeholder="Title" className="title" value={userTitle.Title} onChange={handleTitle}/>
-                <textarea>Description</textarea>
+                <textarea type="text" placeholder="Description" value={userTitle.Desc} onChange={handleDesc}>Description</textarea>
                 <div className="Buttons">
                     <button className="button">Cancel</button>
                     <button className="button" onClick={handleTitleData}>Save</button>
